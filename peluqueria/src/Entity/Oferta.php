4e3b0b47e653baae284cbdb7a013c8a2a58bcb39
frcedermazGH <hrnnnadal@gmail.com>
2024-10-15 19:16:44 -0300

    Primer commit del proyecto

---
+++ peluqueria/src/Entity/Oferta.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OfertaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OfertaRepository::class)]
+class Oferta
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $imagen = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImagen(): ?string
+    {
+        return $this->imagen;
+    }
+
+    public function setImagen(string $imagen): static
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+}
